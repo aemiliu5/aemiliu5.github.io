c0e5ab6cabdbaa4de0eb9f9dc7213f8f8aadcf17
Emilios Seimenis <seimenace@gmail.com>
2019-12-01 19:11:07 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aemiliu5.github.io
-My personal site and portfolio. Currently under construction.
+My personal site and portfolio.
+
+Coded in scratch using HTML/CSS/JS & jQuery.
---
+++ js/navfade.js
@@ -0,0 +1,10 @@
+// Navbar fade
+$(document).ready(function(){ $("#logo").fadeIn(1500); });
+$(document).ready(function(){ $("#nav1").delay(500).fadeIn(1500); });
+$(document).ready(function(){ $("#nav2").delay(1000).fadeIn(1500); });
+$(document).ready(function(){ $("#nav3").delay(1500).fadeIn(1500); });
+
+if(document.title == "Æmilius | Home")
+{
+	$(document).ready(function(){ $("#hometext").fadeIn(3000); });
+}
